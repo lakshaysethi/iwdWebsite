fa56ed02a19a889d734e3773a92bc9924ca06e3f
Lakshay <lakshaysethi.nz@gmail.com>
2019-04-18 22:46:10 +1200

    Update script.js

--- project2/script.js
+++ project2/script.js
@@ -5,8 +5,19 @@
 //showTimes
 var welcomeScreen = document.getElementsByClassName('welcomeScreen');
 var registerScreen =document.getElementsByClassName('registerScreen');
- 
+var loginScreen = document.getElementsByClassName('loginScreen');
+
+
 function showLoginScreen(){
 	welcomeScreen[0].style.display="none";
 	registerScreen[0].style.display="none";
+	loginScreen[0].style.display="block";
 }
+
+
+function showRegisterScreen(){
+	welcomeScreen[0].style.display="none";
+	registerScreen[0].style.display="block";
+}
+
+
